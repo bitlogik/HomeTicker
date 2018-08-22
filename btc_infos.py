a5e68a67a8a24398f7bf4136d15e18fc4a9693bf
Antoine FERRON <antoine.ferron@bitlogik.fr>
2018-08-22 22:16:49 +0200

    Improve Bitcoin Infos
    Lightning total channels added

--- btc_infos.py
+++ btc_infos.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+Your_Currency = "EUR"
+
 import time
 import RESTapi
 import HomeTicker
@@ -23,26 +25,39 @@ print "PRESS CTRL+C TO QUIT"
 myticker = HomeTicker.HomeTicker()
 price_api = RESTapi.getRestJSON('https://api.coindesk.com/v1/bpi/currentprice.json')
 latestblock = RESTapi.getRestJSON('https://chain.api.btc.com/v3/block/latest')
+lntot_api = RESTapi.getRestJSON('https://p2sh.info/api/datasources/proxy/1/query')
+lntot_api.addParam({"db":"p2shinfo","q":'SELECT last("value") FROM "ln_stats" WHERE time > now()-1h GROUP BY time(0h) fill(null)'})
 
 def printprice(curr):
     price_api.getData()
     latestblock.getData()
+    lntot_api.getData()
     btc_price = float( price_api.getKey("bpi/"+curr+"/rate").replace(",", ""))
     myticker.clear_screen()
     myticker.write(("Bitcoin Price\r\n1 BTC = %.2f "+curr)%btc_price)
     time.sleep(10)
     myticker.clear_screen()
+    myticker.write("Bitcoin Stats")
     height = latestblock.getKey("data/height")
-    myticker.write("Height:  %s"%height)
     myticker.pos_cursor(2,1)
+    myticker.write("Height:  %s"%height)
+    time.sleep(5)
     diff = int(latestblock.getKey("data/difficulty"))
+    myticker.pos_cursor(2,1)
+    myticker.clear_line()
     myticker.write("Diff  :  %i G"% (diff/10**9) )
+    time.sleep(5)
+    lntot = float( lntot_api.getKey("results/0/series/0/values/0/1") ) / 100000000
+    myticker.clear_screen()
+    myticker.write("Total LN channels")
+    myticker.pos_cursor(2,4)
+    myticker.write( "   %.1f BTC   " % lntot )
 
 while True:
     try:
         printprice("USD")
         time.sleep(5)
-        printprice("EUR")
+        printprice(Your_Currency)
         time.sleep(5)
     except KeyboardInterrupt:
         break
