3280c1e662703ce8f2bfcff54e9c3ad467991a6b
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2022-08-21 20:44:30 +0200

    Remove dead ph2shinfo txstats for LN stats in btc_infos

--- btc_infos.py
+++ btc_infos.py
@@ -25,13 +25,10 @@ print "PRESS CTRL+C TO QUIT"
 myticker = HomeTicker.HomeTicker()
 price_api = RESTapi.getRestJSON('https://api.coindesk.com/v1/bpi/currentprice.json')
 latestblock = RESTapi.getRestJSON('https://api.blockchair.com/bitcoin/stats')
-lntot_api = RESTapi.getRestJSON('https://txstats.com/api/datasources/proxy/1/query')
-lntot_api.addParam({"db":"p2shinfo","q":'SELECT last("value") FROM "ln_stats" WHERE time > now()-1h GROUP BY time(0h) fill(null)'})
 
 def printprice(curr):
     price_api.getData()
     latestblock.getData()
-    lntot_api.getData()
     btc_price = float( price_api.getKey("bpi/"+curr+"/rate").replace(",", ""))
     myticker.clear_screen()
     myticker.write(("Bitcoin Price\r\n1 BTC = %.2f "+curr)%btc_price)
@@ -47,11 +44,6 @@ def printprice(curr):
     myticker.clear_line()
     myticker.write("Diff  :  %i G"% (diff/10**9) )
     time.sleep(5)
-    lntot = float( lntot_api.getKey("results/0/series/0/values/0/1") ) / 100000000
-    myticker.clear_screen()
-    myticker.write("Total LN channels")
-    myticker.pos_cursor(2,4)
-    myticker.write( "   %.1f BTC   " % lntot )
 
 while True:
     try:
